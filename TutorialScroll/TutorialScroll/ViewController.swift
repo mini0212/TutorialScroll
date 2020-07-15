6e29789f3ddfcbedc84e10fc6f28fdda9a434375
Seongmin <>
2020-07-15 15:38:01 +0900

    Initial Commit

---
+++ TutorialScroll/TutorialScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialScroll
+//
+//  Created by 김성민 on 2020/07/15.
+//  Copyright © 2020 min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
